df30b021b67b0562735c635491001b180f521c90
PetrovskaM <petrovska.martinaa@gmail.com>
2021-04-22 21:29:17 +0200

    Code Improvement

--- covid.js
+++ covid.js
@@ -3,86 +3,110 @@ const countrySelected = document.querySelector('.country-selected');
 const confirmed = document.querySelector('.confirmed');
 const recovered = document.querySelector('.recovered');
 const deaths = document.querySelector('.deaths');
-
-// Searched Country
-searchedButton.addEventListener('click', function () {
-  const countryName = document.querySelector('.search-country').value;
-  localStorage.setItem('country-name', countryName);
-})
-
-const country = localStorage.getItem('country-name');
-countrySelected.innerHTML = country;
-const urlCountryByName = `https://covid-api.mmediagroup.fr/v1/cases?country=${country}`;
-const modalTitle = document.querySelector('.modal-title');
-modalTitle.innerHTML = `${country}`;
+let countryName = document.querySelector('.search-country');
 const modalRecovered = document.querySelector('.modal-recovered');
 const modalDeaths = document.querySelector('.modal-deaths');
 const modalConfirmed = document.querySelector('.modal-confirmed');
-
-// Fetch-Modal $ flag icon
-fetch(urlCountryByName)
-  .then((resp) => resp.json())
-  .then(function (data) {
-    const country = data.All;
-    const flag = `${country.abbreviation}`;
-    const img = document.createElement('img');
-    img.src = `https://www.countryflags.io/${flag}/flat/64.png`;
-    img.setAttribute('class', 'ps-1')
-    countrySelected.appendChild(img);
-    confirmed.innerHTML = `<p class="ps-2 pt-2"> ${country.confirmed}</p>`
-    recovered.innerHTML = `<p class="ps-2 pt-2"> ${country.recovered}</p>`
-    deaths.innerHTML = `<p class="ps-2 pt-2">${country.deaths}</p>`
-    modalRecovered.innerHTML = `Recovered | ${country.confirmed}`;
-    modalDeaths.innerHTML = `Deaths | ${country.deaths}`;
-    modalConfirmed.innerHTML = `Confirmed | ${country.confirmed}`;
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
-const urlHistoryDeath = `https://covid-api.mmediagroup.fr/v1/history?country=${country}&status=deaths`;
-const urlHistoryConfirmed = `https://covid-api.mmediagroup.fr/v1/history?country=${country}&status=confirmed`;
 const deathsYesterday = document.querySelector('.deaths-yesterday');
 const confirmedYesterday = document.querySelector('.confirmed-yesterday');
+const urlMoreCountries = 'https://covid-api.mmediagroup.fr/v1/vaccines';
+const tableBody = document.querySelector('.table-body');
 
-// Fetch History Death
-fetch(urlHistoryDeath).then((resp) => resp.json())
-  .then(function (data) {
-    const country = data.All.dates;
-    deathsYesterday.innerHTML = `<p class="m-0 py-2"> <strong>${date()}</strong></p><p>${country[date()]}</p>`;
+// Searched Country
+searchedButton.addEventListener('click', function () {
+  countryName = countryName.value;
+  searchCountryFetch();
+})
 
+// Searched country-fetch
+searchCountryFetch = () => {
+  const urlCountryByName = `https://covid-api.mmediagroup.fr/v1/cases?country=${countryName}`;
+
+  fetch(urlCountryByName)
+    .then((resp) => resp.json())
+    .then(function (data) {
+      cardsFlagCountryName(data);
+      modalHelpFunc(data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+// Showing searched country flag and cards
+cardsFlagCountryName = (data) => {
+  countrySelected.innerHTML = countryName;
+  const flag = `${data.All.abbreviation}`;
+  const img = document.createElement('img');
+  img.src = `https://www.countryflags.io/${flag}/flat/64.png`;
+  countrySelected.appendChild(img);
+  confirmed.innerHTML = `<p class="ps-2 pt-2"> ${data.All.confirmed}</p>`
+  recovered.innerHTML = `<p class="ps-2 pt-2"> ${data.All.recovered}</p>`
+  deaths.innerHTML = `<p class="ps-2 pt-2">${data.All.deaths}</p>`
+}
+
+// Modal-Help Function
+modalHelpFunc = (data) => {
+  const modalTitle = document.querySelector('.modal-title');
+  modalTitle.innerHTML = `${countryName}`;
+  modalRecovered.innerHTML = `Recovered | ${data.All.confirmed}`;
+  modalDeaths.innerHTML = `Deaths | ${data.All.deaths}`;
+  modalConfirmed.innerHTML = `Confirmed | ${data.All.confirmed}`;
+}
+
+// Arrow Button Eventlistener (showing Modal)
+const arrowButton = document.querySelectorAll('.arrow-button');
+arrowButton.forEach((element) => {
+  element.addEventListener('click', function () {
+    histiryConfirmedModalFetch();
+    historyDeathModalFetch();
   })
-  .catch(function (error) {
-    console.log(error);
-  });
+})
 
-// Fetch History Confirmed
-fetch(urlHistoryConfirmed).then((resp) => resp.json())
-  .then(function (data) {
-    const country = data.All.dates;
-    confirmedYesterday.innerHTML = `<p class="m-0 py-2"> <strong>${date()}</strong></p><p>${country[date()]}</p>`;
+// Fetch History Death-Modal
+historyDeathModalFetch = () => {
+  const urlHistoryDeath = `https://covid-api.mmediagroup.fr/v1/history?country=${countryName}&status=deaths`;
+  fetch(urlHistoryDeath).then((resp) => resp.json())
+    .then(function (data) {
+      deathsYesterday.innerHTML = `<p class="m-0 py-2"> <strong>${date()}</strong></p><p>${data.All.dates[date()]}</p>`;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
 
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+// Fetch History Confirmed-Modal
+histiryConfirmedModalFetch = () => {
+  const urlHistoryConfirmed = `https://covid-api.mmediagroup.fr/v1/history?country=${countryName}&status=confirmed`;
+  fetch(urlHistoryConfirmed).then((resp) => resp.json())
+    .then(function (data) {
+      confirmedYesterday.innerHTML = `<p class="m-0 py-2"> <strong>${date()}</strong></p><p>${data.All.dates[date()]}</p>`;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
 
 // Fetch - Table
-const urlMoreCountries = 'https://covid-api.mmediagroup.fr/v1/vaccines';
-const tableBody = document.querySelector('.table-body')
-
 fetch(urlMoreCountries).then((resp) => resp.json())
   .then(function (data) {
-    let countries = ['France', 'Italy', 'Canada', 'Brazil', 'Croatia', 'Slovenia', 'North Macedonia', 'India', 'Russia', 'Mexico'];
-    countries.forEach(index => {
-      let tr = document.createElement('tr');
-      const updated = `${data[index].All.updated}`;
-      const updated2 = updated.substring(0, 10);
-      tr.innerHTML = `<td> ${data[index].All.country}</td> <td>${data[index].All.administered}</td> <td>${data[index].All.people_vaccinated}</td> <td>${data[index].All.people_partially_vaccinated}</td> <td>${updated2}</td> <td>${data[index].All.population}</td>`;
-      tableBody.appendChild(tr);
-    })
+    tableHelpFunc(data);
   })
+  .catch(function (error) {
+    console.log(error);
+  });
 
+// Table - Help function
+tableHelpFunc = (data) => {
+  let countries = ['France', 'Italy', 'Canada', 'Brazil', 'Croatia', 'Slovenia', 'North Macedonia', 'India', 'Russia', 'Mexico'];
+  countries.forEach(index => {
+    let tr = document.createElement('tr');
+    const updated = `${data[index].All.updated}`;
+    const updated2 = updated.substring(0, 10);
+    tr.innerHTML = `<td> ${data[index].All.country}</td> <td>${data[index].All.administered}</td> <td>${data[index].All.people_vaccinated}</td> <td>${data[index].All.people_partially_vaccinated}</td> <td>${updated2}</td> <td>${data[index].All.population}</td>`;
+    tableBody.appendChild(tr);
+  })
+}
 
 // footer date now
 const d = new Date()
